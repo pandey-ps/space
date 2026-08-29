c98546a14dd7156bf22a5aad13156d84adb3c64e
Prashant Pandey <prashantpandeyiuet@gmail.com>
2026-08-29 18:40:43 +0530

    reduction methods

---
+++ src/reduce.py
@@ -0,0 +1,11 @@
+def reduce(embeddings, method="umap"):
+    if method == "umap":
+        from umap import UMAP
+        return UMAP(n_components=2).fit_transform(embeddings)
+    if method == "tsne":
+        from sklearn.manifold import TSNE
+        return TSNE(n_components=2).fit_transform(embeddings)
+    if method == "pca":
+        from sklearn.decomposition import PCA
+        return PCA(n_components=2).fit_transform(embeddings)
+    raise ValueError(f"unknown method: {method}")
